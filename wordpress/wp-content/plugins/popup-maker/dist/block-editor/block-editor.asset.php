012f652f4878696852a231a3ae85b956ecbb02f2
Haidara Bassidiki <hbassidiki@gmail.com>
2021-01-11 23:27:28 +0100

    ajout partiel resume article et popup mission

---
+++ wordpress/wp-content/plugins/popup-maker/dist/block-editor/block-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-rich-text'), 'version' => '3d33fe49af37c978e0de0d5f57e48f0c');
